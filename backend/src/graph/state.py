b925d49bb0944ed2a9dab63358860e13e0d00e65
Shashank Manu Rao <shashank@Shashanks-MacBook-Pro.local>
2026-06-04 22:31:09 +0530

    state of langgraph added

---
+++ backend/src/graph/state.py
@@ -0,0 +1,27 @@
+import operator
+from typing import Annotated, List, Dict, Optional, Any, TypedDict
+
+class ComplianceIssue(TypedDict):
+    category: str
+    description: str
+    severity: str
+    timestamp: Optional[str]
+
+class VideoAuditState(TypedDict):
+    '''
+    Defines the data schema for langgraph execution content
+    '''
+    video_url: str
+    video_id: str
+
+    local_file_path: Optional[str]
+    video_metadata: Dict[str,Any]
+    transcript: Optional[str]
+    ocr_text: List[str]
+
+    compliance_results: Annotated[List[ComplianceIssue], operator.add]
+
+    final_status: str
+    final_report: str
+
+    errors: Annotated[List[str], operator.add]
