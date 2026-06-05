3e47b8d1a880652f3fdfd49524b98c2d12b8410f
Shashank Manu Rao <shashank@Shashanks-MacBook-Pro.local>
2026-06-05 18:45:17 +0530

    main.py file updated

--- backend/src/services/video_indexer.py
+++ backend/src/services/video_indexer.py
@@ -51,10 +51,14 @@ class VideoIndexerService:
         logger.info(f"Downloading Youtube video: {url}")
 
         ydl_opts = {
-            "format": 'best[ext-mp4]',
+            "format": 'best',
             'outtmpl': output_path,
-            'quiet': True,
-            'overwrites': True
+            'quiet': False,
+            'no_warnings': False,
+            'extractor_args': {'youtube': {'player_client': ['android','web']}},
+            'http_headers': {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'
+            }
         }
         try:
             with yt_dlp.YoutubeDL(ydl_opts) as ydl:
--- main.py
+++ main.py
@@ -1,6 +1,63 @@
-def main():
-    print("Hello from complianceqapipeline!")
+'''
+Main execution entry point for the Compliance QA Pipeline
+'''
+import uuid
+import json
+import logging
+from pprint import pprint
+from dotenv import load_dotenv
+load_dotenv(override=True)
+
+from backend.src.graph.workflow import app
+
+logging.basicConfig(
+    level = logging.INFO,
+    format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+logger = logging.getLogger("brand-guardina-runner")
+
+def run_cli_simulation():
+    '''
+    Simulates the video compliance audit request
+    '''
+    session_id = str(uuid.uuid4())
+    logger.info(f"Starting Audit Session: {session_id}")
+
+    initial_inputs = {
+        "video_url": "https://youtu.be/dT7S75eYhcQ",
+        "video_id": f"vid_{session_id[:8]}",
+        "compliance_results": [],
+        "errors": []
+    }
+
+    print("\n-----Initializing workflow------")
+    print(f"Input payload: {json.dumps(initial_inputs, indent=2)}")
+
+    try:
+        final_state = app.invoke(initial_inputs)
+        print("\n------Workflow Execution is complete-----")
+
+        print("\n==Compliance Audit Report ==")
+        print(f"Video ID: {final_state.get('video_id')}")
+        print(f"Status: {final_state.get('final_status')}")
+        results = final_state.get('compliance_results',[])
+        if results:
+            for issue in results:
+                print(f"- [{issue.get('severity')}] [{issue.get('category')}]: [{issue.get('description')}]")
+        else:
+            print("No violations detected")
+        print("\n[FINAL SUMMARY]")
+        print(final_state.get('final_report'))
+
+    except Exception as e:
+        logger.error(f"Workflow Execution Failed: {str(e)}")
+        raise e
+    
+if __name__=="__main__":
+    run_cli_simulation()
+
+  
 
 
-if __name__ == "__main__":
-    main()
+
+
